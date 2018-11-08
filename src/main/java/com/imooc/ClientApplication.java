bd11d8ce1fadbb08790bba8aa16d1f97dfa6f41d
aidon0818 <30116992@qq.com>
2018-11-08 14:12:59 +0800

    添加一个action方法

--- src/main/java/com/imooc/ClientApplication.java
+++ src/main/java/com/imooc/ClientApplication.java
@@ -3,12 +3,20 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hoem() {
+        return "hello world from port";
+    }
 }
